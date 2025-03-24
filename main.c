df4150485867303a69f727f419302324944679cb
Junghyeon Ryu <rjh0162@gmail.com>
2025-03-24 14:37:03 +0900

    message [test] : 새 브렌치 생성성

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main(void){
-    printf("Hello 202503 24-14:28")
+    printf("Hello 202503 24-14:28");
+    return 0
+
+}
+
+int sayhello(void){
+    printf("Heello ello ello ello llo ello ello ello ellello o ");
     return 0
 
 }
