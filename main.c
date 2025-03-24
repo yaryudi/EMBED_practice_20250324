83338ed03707f1f4613fc3668ab73fb312b7569b
Junghyeon Ryu <rjh0162@gmail.com>
2025-03-24 14:44:46 +0900

    함수변경 [test] : sayhello 함수 변경경

--- main.c
+++ main.c
@@ -2,12 +2,11 @@
 
 int main(void){
     printf("Hello 202503 24-14:28");
-    return 0
+    return 0;
 
 }
 
-int sayhello(void){
-    printf("Heello ello ello ello llo ello ello ello ellello o ");
-    return 0
+int sayhello(int a, int b){
+    return a + b;
 
 }
